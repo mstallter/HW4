15a64c290aad1f2ad7d30a13698c8e52e36e9595
Michael Stallter <mstallter@comcast.net>
2020-03-23 17:15:40 -0500

    created form after video 1

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # SQL SCRIPT TO CREATE AND ADD TO THE TABLE PRESIDENTS
 
 
-CREATE TABLE Presidents(
+CREATE TABLE mstallter_Presidents(
+	NUM int not null,
 	FIRSTNAME varchar(255),
 	LASTNAME varchar(255),
 	AGE int,
-	STATE varchar(255),
-	CITY varchar(255),
+	HOMESTATE varchar(255),
+	HOMECITY varchar(255),
 	TERMS int,
-    YROFFRSTTERM int
+    YROFFRSTTERM int,
+	primary key (NUM)
 );
 
-INSERT INTO Presidents
-VALUES("Donald", "Trump", 73, "New York", "New York", 1, 2016),
-("Barack", "Obama", 58, "Hawaii", "Honolulu", 2, 2008),
-("George", "Bush", 73, "Connecticut", "New Haven", 2, 2000),
-("Bill", "Clinton", 73, "Arkansas", "Hope", 2, 1992),
-("George HW", "Bush", 94, "California", "Los Angeles", 1, 1988),
-("Ronald", "Reagan", 93, "Illinois", "Tampico", 2, 1980),
-("Jimmy", "Carter", 95, "Georgia", "Plains", 1, 1976);
+INSERT INTO mstallter_Presidents
+VALUES(45, "Donald", "Trump", 73, "New York", "New York", 1, 2016),
+(44, "Barack", "Obama", 58, "Hawaii", "Honolulu", 2, 2008),
+(43, "George", "Bush", 73, "Connecticut", "New Haven", 2, 2000),
+(42, "Bill", "Clinton", 73, "Arkansas", "Hope", 2, 1992),
+(41, "George HW", "Bush", 94, "California", "Los Angeles", 1, 1988),
+(40, "Ronald", "Reagan", 93, "Illinois", "Tampico", 2, 1980),
+(39, "Jimmy", "Carter", 95, "Georgia", "Plains", 1, 1976);
 
--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 from flask import Flask
 from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from wtforms import IntegerField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SuperSecretKey'
+
+class PresForm(FlaskForm):
+    num = IntegerField('President Number:', validators=[DataRequired()])
+    first_name = StringField('First name:', validators=[DataRequired()])
+    last_name = StringField('Last name:', validators=[DataRequired()])
+    age = IntegerField('Current Age:', validators=[DataRequired()])
+    home_state = StringField('Home state:', validators=[DataRequired()])
+    home_city = StringField('Home city:', validators=[DataRequired()])
+    terms = IntegerField('Number of terms:', validators=[DataRequired()])
+    first_term = IntegerField('Year inaugurated:', validators=[DataRequired()])
+
+    
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+    return render_template('index.html', pageTitle='America\'s Presidents')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html', pageTitle='About Mike')
+@app.route('/add_pres', methods=['GET', 'POST'])
+def add_pres():
+    form = PresForm()
+    if form.validate_on_submit():
+        return "<h2> {0} {1} was President number {2}, from {3}, {4}. They served {5} term(s) starting in {6}, and are currently {7} years old.".format(form.first_name.data, form.last_name.data, form.num.data, form.home_city.data, form.home_state.data, form.terms.data, form.first_term.data, form.age.data)
+        
+    return render_template('add_pres.html', form=form, pageTitle="Add a new President")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
